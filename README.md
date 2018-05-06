79827d6c3a203c23ee80fc802e68d6e9eef486e2
Denis Zhuravlev <denis.zhuravlev@Deniss-MacBook-Pro.local>
2018-05-06 16:24:33 -0300

    added -v (statics) and max line size

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 # pipe-size-fuse
+This small app allows to limit pipe size, the same as "cstream -n" or "pv --stop-at-size --size" but it raise an error (exit 1).
+Also it can raise an error on long strings (EOL).
 ```
 $ gcc -o pfuse pipe-size-fuse.c
 
 $ ./pfuse -?
 usage: ./pfuse -s bytes -L bytes
  -s -- max pipe size [kMG],
- -L -- max line size[kMG].
+ -L -- max line size [kMG] (eol is any cobmination of \r and \n, all other bytes don't matter). Makes a pipe much slower!.
+ -v -- show pipe statistics (stderr).
 
 $ dd if=/dev/zero bs=1m count=6000|./pfuse -s1G  >/dev/null
 Error. The pipe size 1074003968 exceeded the limit 1073741824 bytes
 
 $ echo -e "rrrrrr\n\rffff"|./pfuse -L 2
-Error. The line size  3 exceeded the limit 2 bytes
+Error. Line 1 exceeded the limit 2 bytes
+
+$ cat test.log|./pfuse -s 20G -L 5M -v >/dev/null
+Pipe fuse size: 21474836480
+Line fuse size: 5242880
+Max line: 14410
+Lines: 1292
+```
+
+-L works up to 10 times slower: 
+```
+$ time cat ~/Downloads/ubuntu-18.04-desktop-amd64.iso | ./pfuse -s 20G >/dev/null
+real	0m0.871s
+user	0m0.053s
+sys	0m0.881s
+
+$ time cat ~/Downloads/ubuntu-18.04-desktop-amd64.iso | ./pfuse -s 20G -L 10G >/dev/null
+real	0m6.591s
+user	0m5.082s
+sys	0m1.234s
 ```
--- pipe-size-fuse.c
+++ pipe-size-fuse.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #define BLOCK_SIZE 262144 // 256k buffer
 
 #define t_ulli unsigned long long int
-
+#define new_max(x,y) ((x) >= (y)) ? (x) : (y)
 
 t_ulli parse_human_readable_byte_size(char *s) {
-  char *endp = s;
+  char *endp;// = s;
   int sh;
   int errno = 0;
   t_ulli x = strtoull(s, &endp, 10);
@@ -25,10 +24,10 @@ t_ulli parse_human_readable_byte_size(char *s) {
   return x;
 }
 
-int parseargs(int argc, char **argv, t_ulli *svalue, t_ulli *lvalue) {
+int parseargs(int argc, char **argv, t_ulli *svalue, t_ulli *lvalue, int *vinfo) {
   int c;
 
-  while ((c = getopt (argc, argv, "?s:L:")) != -1)
+  while ((c = getopt (argc, argv, "v?s:L:")) != -1)
     switch (c)
       {
       case 's':
@@ -39,9 +38,16 @@ int parseargs(int argc, char **argv, t_ulli *svalue, t_ulli *lvalue) {
         *lvalue = parse_human_readable_byte_size(optarg);
         //*lvalue = atoll(optarg);
         break;
+      case 'v':
+        *vinfo = 1;
+        break;
       case '?':
-        fprintf (stderr, "usage: %s -s bytes -L bytes \n ", argv[0]);
-        fprintf (stderr, "-s -- max pipe size [kMG], \n -L -- max line size[kMG].\n");
+        fprintf (stderr, "usage: %s -s bytes -L bytes \n", argv[0]);
+        fprintf (stderr, " -s -- max pipe size [kMG], \n"
+                         " -L -- max line size [kMG]"
+                         " (eol is any cobmination of \\r and \\n, all other bytes"
+                         " don't matter). Makes a pipe much slower!.\n"
+                         " -v -- show pipe statistics (stderr).\n");
         return 1;
       default:
         abort ();
@@ -56,30 +62,39 @@ int main(int argc, char **argv)
 {
   t_ulli pipe_fuse_size = -1;
   t_ulli line_fuse_size = -1;
-
-  if (parseargs(argc, argv, &pipe_fuse_size, &line_fuse_size) != 0) return 1;
+  int vinfo = 0;
+  if (parseargs(argc, argv, &pipe_fuse_size, &line_fuse_size, &vinfo) != 0) return 1;
 
 
   char buffer[BLOCK_SIZE];
 
   t_ulli pipe_size = 0;
   t_ulli line_size = 0;
+  t_ulli line_number = 0;
+
+  t_ulli max_line_size = 0;
+  //char last_char;
 
   for(;;) {
     size_t bytes = fread(buffer, 1, BLOCK_SIZE, stdin);
     pipe_size += bytes;
     if (pipe_size > pipe_fuse_size) {
-      fprintf( stderr, "Error. The pipe size %llu exceeded the limit %llu bytes\n", pipe_size, pipe_fuse_size);
+      fprintf(stderr, "Error. The pipe size %llu exceeded the limit %llu bytes\n", pipe_size, pipe_fuse_size);
       return 1;
     }
 
-    if (line_fuse_size != -1) {
-      int i;
-      for (i = 0; i < bytes; ++i) {
+    if (line_fuse_size != -1 || vinfo) {
+      for (int i = 0; i < bytes; ++i) {
         line_size ++;
-        if ('\r' == buffer[i] || '\n' == buffer[i]) line_size = 0;
+        if ('\r' == buffer[i] || '\n' == buffer[i]) {
+          //if (! ('\r' == last_char && '\n' == buffer[i]))
+          line_number ++; //if : check for two character winEOL (\r\n)
+          max_line_size = new_max(max_line_size, line_size - 1);
+          line_size = 0;
+        }
+        //last_char = buffer[i];
         if (line_size > line_fuse_size) {
-          fprintf( stderr, "Error. The line size  %llu exceeded the limit %llu bytes\n", line_size, line_fuse_size);
+          fprintf(stderr, "Error. Line %llu exceeded the limit %llu bytes\n", line_number+1, line_fuse_size);
           return 1;
         }
       }
@@ -88,8 +103,17 @@ int main(int argc, char **argv)
     fwrite(buffer, 1, bytes, stdout);
     fflush(stdout);
     if (bytes < BLOCK_SIZE)
-      if (feof(stdin)) break;
+      if (feof(stdin)) {
+        max_line_size = new_max(max_line_size, line_size);
+        break;
+      }
 
+  }
+  if (vinfo) {
+    fprintf(stderr, "Pipe fuse size: %llu \n", pipe_fuse_size);
+    fprintf(stderr, "Line fuse size: %llu \n", line_fuse_size);
+    fprintf(stderr, "Max line: %llu \n", max_line_size);
+    fprintf(stderr, "Lines: %llu \n", line_number);
   }
   return 0;
 }
